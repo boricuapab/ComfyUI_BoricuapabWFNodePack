d05517e84cf091ac1ef590ecb3605ebee2e330b1
boricuapab <61924816+boricuapab@users.noreply.github.com>
2024-07-07 21:56:41 -0700

    add concat
    add concat

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 
-from .nodes.boricuapabWF_nodesHelloWorld import *
-from .nodes.boricuapabWF_nodesElNeneDeTiti import *
+from .nodes.boricuapabWF_nodes import *
+#from .nodes.boricuapabWF_nodesElNeneDeTiti import *
 
 NODE_CLASS_MAPPINGS = {
     # Add mappings here
-    "Print BoricuapabWF Hello World": PrintBoricuapabWFHelloWorld,
-    "Print BoricuapabWF El Nene De Titi": PrintBoricuapabWFElNeneDeTiti,
+    "BoricuapabWF Print Hello Puerto Rican World": BoricuapabWFPrintHelloPuertoRicanWorld,
+    "BoricuapabWF Print Puerto Rican": BoricuapabWFPrintPuertoRican,
+    "BoricuapabWF Concatenate Hello World": BoricuapabWFConcatenateHelloWorld,
 }
 
 print("\033[34mComfyUI_BoricuapabWFNodePack Nodes: \033[92mLoaded\033[0m")
---
+++ nodes/boricuapabWF_nodes.py
@@ -0,0 +1,59 @@
+
+class BoricuapabWFPrintHelloPuertoRicanWorld:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+
+        return {"required":{
+                    "text": ("STRING", {"multiline": False, "default": "BoricuapabWF Hello Puerto Rican World"}),
+                    }
+                }
+    
+    RETURN_TYPES = ()
+    FUNCTION = "print_BoricuapabWFHWText"
+    OUTPUT_NODE = True
+    CATEGORY = "BoricuapabWF Nodes"
+
+    def print_BoricuapabWFHWText(self, text):
+        print(f"BoricuapabWF HW Text : {text}")
+
+        return{}
+    
+class BoricuapabWFPrintPuertoRican:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+
+        return {"required":{
+                    "text": ("STRING", {"multiline": False, "default": "BoricuapabWF Puerto Rican"}),
+                    }
+                }
+    
+    RETURN_TYPES = ()
+    FUNCTION = "print_BoricuapabWFPuertoRican"
+    OUTPUT_NODE = True
+    CATEGORY = "BoricuapabWF Nodes"
+
+    def print_BoricuapabWFPuertoRican(self, text):
+        print(f"BoricuapabWF Puerto Rican Text : {text}")
+
+        return{}
+
+class BoricuapabWFConcatenateHelloWorld:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+
+        return {"required": {
+                    "textLine1": ("STRING", {"multiline": False, "default": "Hello Puerto"}),
+                    "textLine2": ("STRING", {"multiline": False, "default": "Rican World"}),
+                    }            
+                }
+
+    RETURN_TYPES = ("STRING", )
+    FUNCTION = "BWF_concatenate_text"
+    CATEGORY = "BoricuapabWF Nodes"
+
+    def BWF_concatenate_text (self, textLine1, textLine2):
+        text_out = textLine1 + " " + textLine2
+        return(text_out, )
